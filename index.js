7ad92f9064cb129d5946ea8bd8f0da978b2d580d
arjundangi01 <arjundangi907473@gmail.com>
2023-10-20 11:30:09 +0530

    link added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT
 const app = express();
 
 app.use(cors({
-    origin:['http://localhost:3000']
+    origin:['http://localhost:3000','https://travelious-project.netlify.app']
 }))
 app.use(express.json())
 
